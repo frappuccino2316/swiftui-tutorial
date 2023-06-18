2eda7f03411564628fbc95a95b5332278a2e1372
frappuccino2316 <frappuccino2316@gmail.com>
2023-06-18 22:35:10 +0900

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by 髙木文弘 on 2023/06/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 髙木文弘 on 2023/06/18.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
