b56909604603dfb7c28385bf97d8d4e1d3e7ad89
frappuccino2316 <frappuccino2316@gmail.com>
2023-06-20 01:23:20 +0900

    tutorial ch 4

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,26 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by 髙木文弘 on 2023/06/20.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.gray, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
+
